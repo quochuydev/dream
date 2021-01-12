d308345bdf7a1adebfa0344aa5d5d4442ed2eee8
quochuydev <quochuy.dev@gmail.com>
2021-01-12 10:53:33 +0700

    UPDATE

--- client/api/index.js
+++ client/api/index.js
@@ -1,6 +1,13 @@
 import Fetch from "./fetch";
 
-const API = new Fetch("http://localhost:3000", {
+console.log(process.env.BACKEND_URL);
+
+const BACKEND_URL =
+  process.env.NODE_ENV == "production"
+    ? process.env.BACKEND_URL
+    : "http://localhost:3000";
+
+const API = new Fetch(BACKEND_URL, {
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
