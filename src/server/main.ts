874545317edefa27f2a5f1f5baf2a95f674f82ff
huy.pham@inapps.net <huy.pham@inapps.net>
2021-01-19 17:36:59 +0700

    UPDATE

--- src/server/main.ts
+++ src/server/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  const PORT = process.env.PORT || 3000;
+  console.log({ PORT });
+  await app.listen(PORT);
 }
 bootstrap();
