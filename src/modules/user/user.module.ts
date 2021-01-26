9a6616a34b95d6d4a67f5b3445c0a6797c25d4d1
quochuydev <quochuy.dev@gmail.com>
2021-01-26 15:16:52 +0700

    UPDATE

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,15 +1,15 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 
-import { FileController } from "./file.controller";
-import { FileService } from "./file.service";
-import { File, FileSchema } from "./file.schema";
+import { UserController } from "./user.controller";
+import { UserService } from "./user.service";
+import { User, UserSchema } from "./user.schema";
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: File.name, schema: FileSchema }]),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
-  providers: [FileService],
-  controllers: [FileController],
+  providers: [UserService],
+  controllers: [UserController],
 })
-export class FileModule {}
+export class UserModule {}
