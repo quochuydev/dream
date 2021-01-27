072123314f998e1bf212a6309b733060ffbaa652
quochuydev <quochuy.dev@gmail.com>
2021-01-27 15:56:59 +0700

    UPDATE

--- client/api/index.js
+++ client/api/index.js
@@ -40,8 +40,9 @@ export const APIFormData = new Fetch(
 function getToken() {
   const token = localStorage.getItem("accessToken");
   if (!token) {
-    return;
+    throw new Error('401')
   }
+  return token;
 }
 
 function setHeader(config) {
