29578b1b141ce45dd47fc6cbdb01d6d247b69304
quochuydev <quochuy.dev@gmail.com>
2021-01-29 23:36:52 +0700

    update

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,7 +2,7 @@ import { noSSRWithLoadingDynamic } from "../utils/dynamic.import";
 
 import { BlogService } from "../services";
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const initQuery = { page: 1, limit: 20 };
   const result = await BlogService.publish.list(initQuery);
 
