91092fe4193c1f5e10f56e39b631fb1ed02845c4
quochuydev <quochuy.dev@gmail.com>
2021-02-05 16:31:59 +0700

    u

--- next.config.js
+++ next.config.js
@@ -6,6 +6,29 @@ module.exports = Object({
     includePaths: [path.join(__dirname, "styles")],
   },
   env: {
-    // BACKEND_URL: process.env.BACKEND_URL,
+    BACKEND_URL: process.env.BACKEND_URL,
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    if (isServer) {
+      const antStyles = /antd\/.*?\/style\/css.*?/;
+      const origExternals = [...config.externals];
+      config.externals = [
+        (context, request, callback) => {
+          if (request.match(antStyles)) return callback();
+          if (typeof origExternals[0] === "function") {
+            origExternals[0](context, request, callback);
+          } else {
+            callback();
+          }
+        },
+        ...(typeof origExternals[0] === "function" ? [] : origExternals),
+      ];
+
+      config.module.rules.unshift({
+        test: antStyles,
+        use: "null-loader",
+      });
+    }
+    return config;
   },
 });
