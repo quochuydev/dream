bd2134cc259f0814e8ec77ccdd35faf08000035e
quochuydev <quochuy.dev@gmail.com>
2021-02-05 16:43:46 +0700

    u

--- next.config.js
+++ next.config.js
@@ -9,26 +9,6 @@ module.exports = Object({
     BACKEND_URL: process.env.BACKEND_URL,
   },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    if (isServer) {
-      const antStyles = /antd\/.*?\/style\/css.*?/;
-      const origExternals = [...config.externals];
-      config.externals = [
-        (context, request, callback) => {
-          if (request.match(antStyles)) return callback();
-          if (typeof origExternals[0] === "function") {
-            origExternals[0](context, request, callback);
-          } else {
-            callback();
-          }
-        },
-        ...(typeof origExternals[0] === "function" ? [] : origExternals),
-      ];
-
-      config.module.rules.unshift({
-        test: antStyles,
-        use: "null-loader",
-      });
-    }
     return config;
   },
 });
