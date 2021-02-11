6b27a2404c3ed3bd45ad809875cc4b9f479596d5
quochuydev <quochuy.dev@gmail.com>
2021-02-11 17:01:51 +0700

    update

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -4,7 +4,7 @@ import { CookiesProvider } from "react-cookie";
 
 import Loading from "../components/Loading";
 
-import "./style.css";
+import "../styles/global.css";
 import "../styles/ckeditor.css";
 
 export default function App({ Component, pageProps }) {
