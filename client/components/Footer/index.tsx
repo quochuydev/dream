4502442fed65d3abf3c4c64acb05b5413af7b7ee
quochuydev <quochuy.dev@gmail.com>
2021-02-15 13:20:57 +0700

    update

--- client/components/Footer/index.tsx
+++ client/components/Footer/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Spin, Tag } from "antd";
+import { Spin, Tag, Row, Col } from "antd";
 
 import "antd/dist/antd.css";
 
@@ -9,17 +9,25 @@ export default function Footer({}) {
 
   return (
     <>
-    <h1>GIỚI THIỆU</h1>
+    <Row>
+    <Col span={8}>
+    <h2>GIỚI THIỆU</h2>
     <p>quochuydev@gmail.com</p>
       <p>Email liên hệ: quochuydev@gmail.com</p>
-
-      <h1>TỪ KHÓA NỔI BẬT</h1>
+    </Col>
+    <Col span={8}>
+    <h2>TỪ KHÓA NỔI BẬT</h2>
       {tags.map(tag => <Tag key={tag.value} color="cyan" style={{ marginRight: 5 }}>
           {tag.label}
         </Tag>)}
-
-      <h1>KẾT NỐI VỚI INVESTING</h1>
+    </Col>
+    <Col span={8}>
+    <h2>KẾT NỐI VỚI INVESTING</h2>
       <p>quochuydev - Việt Nam</p>
+    </Col>
+  </Row>
+
+
 
     </>
   );
