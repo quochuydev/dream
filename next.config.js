7493995b6a418fe6afe82af5e192c2b7b5558a4a
quochuydev <quochuy.dev@gmail.com>
2021-02-16 06:39:04 +0700

    i

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-const withCss = require("@zeit/next-css");
+// const withCss = require("@zeit/next-css");
 
-module.exports = withCss({
+module.exports = Object({
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
   env: {
     BACKEND_URL: process.env.BACKEND_URL
   },
