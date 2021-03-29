370fbfeb0ddcb58899c169fb456e85a2ad39287f
quochuydev <quochuy.dev@gmail.com>
2021-03-29 10:27:28 +0700

    UPDATE

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,13 +1,11 @@
 import { UserService } from "./user.service";
 import { Param, Controller, Get, Query, UseGuards } from "@nestjs/common";
-import { JwtGuard, AdminGuard } from "../authA/jwt.guard";
 
 @Controller("/api/users")
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Get()
-  // @UseGuards(AdminGuard)
   async list(@Query() query) {
     return { users: await this.userService.list(query) };
   }
