a4b7d562edd35625a637e269cb7d85c22451b0ce
quochuydev <quochuy.dev@gmail.com>
2021-03-30 16:20:02 +0700

    update

--- client/pages/api/auth/[...nextauth].js
+++ client/pages/api/auth/[...nextauth].js
@@ -4,11 +4,6 @@ import Adapters from "next-auth/adapters";
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
-console.log(
-  "process.env.GOOGLE_ID",
-  process.env.GOOGLE_ID,
-  process.env.DATABASE_URL
-);
 
 export default NextAuth({
   // https://next-auth.js.org/configuration/providers
--- next.config.js
+++ next.config.js
@@ -7,9 +7,8 @@ module.exports = {
   },
   env: {
     BACKEND_URL: process.env.BACKEND_URL,
-    // GOOGLE_ID:
-    //   "423953942314-g5kp3srv5mgv4od7q9rguio724033a29.apps.googleusercontent.com",
-    GOOGLE_ID: process.env.GOOGLE_ID,
+    GOOGLE_ID:
+      "423953942314-g5kp3srv5mgv4od7q9rguio724033a29.apps.googleusercontent.com",
     GOOGLE_SECRET: "TPDX-w0Mzk-_8O13RpaJ9Tcs",
     NEXTAUTH_URL: "http://localhost:3000",
     DATABASE_URL: process.env.DATABASE_URL,
