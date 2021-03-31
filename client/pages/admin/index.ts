a084fca9cbd9bc39c0007139422741a34a139cf1
quochuydev <quochuy.dev@gmail.com>
2021-03-31 23:25:17 +0700

    update

--- client/pages/admin/index.ts
+++ client/pages/admin/index.ts
@@ -1,6 +1,8 @@
 import { getSession } from "next-auth/client";
 import { noSSRWithLoadingDynamic } from "../../utils/dynamic.import";
 
+export default noSSRWithLoadingDynamic(import("../../admin/pages"));
+
 export async function getServerSideProps(context) {
   const session = await getSession(context);
   if (!session) {
