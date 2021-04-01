dd195fb1eac469a2e751960050a0c2794445e025
quochuydev <quochuy.dev@gmail.com>
2021-04-01 17:40:26 +0700

    update

--- client/web/components/MainMenu/index.tsx
+++ client/web/components/MainMenu/index.tsx
@@ -28,16 +28,28 @@ export default function MainMenu({ ...props }) {
         </Link>
       </Menu.Item>
       <SubMenu
-        title={
-          <span className="submenu-title-wrapper">
-            {/* <HomeOutlined /> */}
-            Products
-          </span>
-        }
+        title={<span className="submenu-title-wrapper">HINO TẢI THÙNG</span>}
       >
-        <Menu.Item key="setting:1">T-Shirst</Menu.Item>
-        <Menu.Item key="setting:2">Jean</Menu.Item>
+        <Menu.Item key="collect:11">THÙNG BẠT</Menu.Item>
+        <Menu.Item key="collect:12">THÙNG KÍN</Menu.Item>
+        <Menu.Item key="collect:13">THÙNG LỬNG</Menu.Item>
+        <Menu.Item key="collect:14">THÙNG BẢO ÔN</Menu.Item>
+        <Menu.Item key="collect:15">THÙNG ĐÔNG LẠNH</Menu.Item>
+        <Menu.Item key="collect:16">THÙNG BEN</Menu.Item>
       </SubMenu>
+      <SubMenu
+        title={<span className="submenu-title-wrapper">HINO CHUYÊN DỤNG</span>}
+      >
+        <Menu.Item key="collect:21">HINO GẮN CẨU</Menu.Item>
+        <Menu.Item key="collect:22">HINO CHỞ GIA CẦM</Menu.Item>
+        <Menu.Item key="collect:23">HINO CHỞ GIA SÚC</Menu.Item>
+        <Menu.Item key="collect:24">HINO BỒN XĂNG DẦU</Menu.Item>
+        <Menu.Item key="collect:25">HINO CHỞ XE</Menu.Item>
+        <Menu.Item key="collect:26">HINO ÉP RÁC</Menu.Item>
+        <Menu.Item key="collect:27">HINO CHỞ CÁM</Menu.Item>
+        <Menu.Item key="collect:28">HINO XITEC</Menu.Item>
+      </SubMenu>
+
       <Menu.Item key="blogs">
         <Link href="/blogs">Blogs</Link>
       </Menu.Item>
