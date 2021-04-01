19f5db42661b0c2822991fa1327217ceb97a593a
quochuydev <quochuy.dev@gmail.com>
2021-04-01 13:09:18 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,17 +1,23 @@
 const path = require("path");
 
+console.log(process.env.DATABASE_URL);
+
 module.exports = Object({
   webpack: (config) => {
     config.resolve.alias["@"] = path.resolve(__dirname);
     return config;
   },
   env: {
-    BACKEND_URL: process.env.BACKEND_URL,
+    BACKEND_URL: "https://dreamdlc.herokuapp.com",
     GOOGLE_ID:
       "423953942314-g5kp3srv5mgv4od7q9rguio724033a29.apps.googleusercontent.com",
     GOOGLE_SECRET: "TPDX-w0Mzk-_8O13RpaJ9Tcs",
-    NEXTAUTH_URL: "http://localhost:3000",
-    DATABASE_URL: process.env.DATABASE_URL,
+    NEXTAUTH_URL: "https://dreamdlc.herokuapp.com",
+    DATABASE_URL:
+      "mongodb+srv://quochuydev:Quochuydev548!@cluster0.qcuvq.mongodb.net/dream?retryWrites=true&w=majority",
     SECRET: "",
+  },
+  publicRuntimeConfig: {
+    DATABASE_URL: process.env.DATABASE_URL,
   },
 });
